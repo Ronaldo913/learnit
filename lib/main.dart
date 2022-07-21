e48b080e95aa3e89503b9c9de34267c8fd79b7d8
Ronaldo Noberto <103396752+Ronaldo913@users.noreply.github.com>
2022-07-21 07:38:15 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:learnit/pages/home_page.dart';
+import 'package:learnit/pages/animation.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Homepage(),
+    home: AnimationPage(),
   ));
 }
 
