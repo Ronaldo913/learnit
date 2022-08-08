a31dd4eb86273d69143c88869919fa8d5b2cfa91
mayarinhan <103398087+mayarinhan@users.noreply.github.com>
2022-08-08 10:47:17 -0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,20 +3,60 @@ import 'package:learnit/pages/assuntos_page.dart';
 import 'package:learnit/pages/questoes_page.dart';
 import 'package:learnit/pages/pacote_page.dart';
 
-class Homepage extends StatefulWidget{
-  const Homepage({Key? key}) : super(key:key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<Homepage> createState() => _HomepageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       drawer: Drawer(
-
+        child: ListView(
+          children: [
+            DrawerHeader(
+                child: Text("Menu do Learnit",
+                  style: TextStyle(
+                    fontSize: 24
+                  ),
+                ),
+              decoration: BoxDecoration(
+                color: Colors.cyan
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle,
+              ),
+              title: Text("Perfil",
+                style: TextStyle(
+                  fontSize: 24
+                ),
+              ),
+              subtitle: Text("Perfil do usuário...",
+                style: TextStyle(
+                    fontSize: 18,
+                ),
+              ),
+                trailing: IconButton(
+                  icon: Icon(Icons.account_circle),
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const perfilPage();
+                        },
+                      ),
+                    );
+                  },
+                ),
+            )
+          ],
+        ),
       ),
       appBar: AppBar(
         title: const Text('Learn It + Bio'),
@@ -34,7 +74,7 @@ class _HomepageState extends State<Homepage> {
               children: [
                 Padding(padding: EdgeInsets.all(16),
                   child: ElevatedButton(onPressed: onPressedButton,
-                    child: const Padding(padding: EdgeInsets.symmetric(vertical: 16),
+                    child: const Padding(padding: EdgeInsets.symmetric(vertical: 20),
                       child: const Text(
                         'CONTEÚDOS',
                         style: TextStyle(
@@ -45,12 +85,15 @@ class _HomepageState extends State<Homepage> {
                     ),
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFF125C13),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                      ),
                     ),
                   ),
                 ),
                 Padding(padding: EdgeInsets.all(16),
                   child: ElevatedButton(onPressed: onPressedButton2,
-                    child: const Padding(padding: EdgeInsets.symmetric(vertical: 16),
+                    child: const Padding(padding: EdgeInsets.symmetric(vertical: 20),
                       child: const Text(
                         'QUESTÕES',
                         style: TextStyle(
@@ -61,10 +104,13 @@ class _HomepageState extends State<Homepage> {
                     ),
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFF125C13),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
                     ),
                   ),
                 ),
-                SizedBox(height: 144),
+                SizedBox(height: 24),
                 Padding(padding: EdgeInsets.all(20),
                   child: ElevatedButton(onPressed: onPressedButton3,
                     child: const Padding(padding: EdgeInsets.symmetric(vertical: 24),
@@ -78,6 +124,9 @@ class _HomepageState extends State<Homepage> {
                     ),
                     style: ElevatedButton.styleFrom(
                       primary: Colors.deepPurple[900],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(35.0)
+                      )
                     ),
                   ),
                 ),
