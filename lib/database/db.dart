2f4cfcd9e3f53f6d6644d846e01181df8dc6ee8e
JhenifferMaia <100545025+JhenifferMaia@users.noreply.github.com>
2022-08-20 20:03:23 -0300

    arquivos p/ o banco de dados

---
+++ lib/database/db.dart
@@ -0,0 +1,34 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path/path.dart';
+
+class DB {
+  // Construtor com acesso privado
+  DB._();
+  // Criar uma instancia de DB
+  static final DB instance = DB._();
+  //Instancia do SQLite
+  static Database? _database;
+
+  get database async {
+    if (_database != null) return _database;
+
+    return await _initDatabase();
+  }
+
+  _initDatabase() async {
+    return await openDatabase(
+      join(await getDatabasesPath(), 'usuario.db'),
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  _onCreate(db, version) async {
+    await db.execute(_usuario);
+    await db.insert('usuario', {'assuntos_estudados': 0});
+  }
+
+  String get _usuario =>
+      '''CREATE TABLE usuario (id INTEGER PRIMARY KEY AUTOINCREMENT, assuntos_estudados INTEGER);''';
+}
---
+++ lib/repositories/usuario_repository.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:learnit/database/db.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+import 'package:learnit/models/posicao.dart';
+
+class UsuarioRepository extends ChangeNotifier {
+  late Database db;
+  List<Posicao> _usuario = [];
+  int _assuntos_estudados = 0;
+
+  get assuntos_estudados => _assuntos_estudados;
+  List<Posicao> get usuario => _usuario;
+
+  UsuarioRepository() {
+    _initRepository();
+  }
+
+  _initRepository() async {
+    await _getAssuntos_estudados();
+  }
+
+  _getAssuntos_estudados() async {
+    db = await DB.instance.database;
+    List conta = await db.query('usuario', limit: 1);
+    _usuario = conta.first['assuntos_estudados'];
+    notifyListeners();
+  }
+
+  setAssuntos_estudados(int quantidade) async {
+    db = await DB.instance.database;
+    db.update('usuario', {'assuntos_estudados': quantidade});
+    _assuntos_estudados = quantidade;
+    notifyListeners();
+  }
+}
