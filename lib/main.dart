b466786a50716fb01cab5d344d7e4cc74118470e
JhenifferMaia <100545025+JhenifferMaia@users.noreply.github.com>
2022-08-20 20:11:00 -0300

    main modificada p/ o banco dar certo (nada do fluxo de telas mudado)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:learnit/repositories/usuario_repository.dart';
+import 'package:provider/provider.dart';
 import 'package:learnit/pages/home_page.dart';
 import 'package:learnit/pages/animation.dart';
 import 'package:learnit/pages/login_page.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: AnimationPage(page: Loginpage()),
-  ));
+  runApp(ChangeNotifierProvider(
+      create: (context) => UsuarioRepository(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        /*title: 'Perfil',
+        //theme: ThemeData(
+        //  primarySwatch: Colors.indigo,
+        ),*/
+        home: Loginpage(),
+      )));
 }
