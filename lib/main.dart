58b00e3be4ab6779783259dc48676b5a05f92d7f
JhenifferMaia <100545025+JhenifferMaia@users.noreply.github.com>
2022-08-22 17:00:17 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:learnit/repositories/usuario_repository.dart';
-import 'package:provider/provider.dart';
+//import 'package:learnit/repositories/usuario_repository.dart';
+//import 'package:provider/provider.dart';
 import 'package:learnit/pages/home_page.dart';
 import 'package:learnit/pages/animation.dart';
 import 'package:learnit/pages/login_page.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-      create: (context) => UsuarioRepository(),
-      child: MaterialApp(
+  runApp(
+    //ChangeNotifierProvider(
+     //create: (context) => UsuarioRepository(),
+      //child:
+       MaterialApp(
         debugShowCheckedModeBanner: false,
-        
-        home: AnimationPage(page:Loginpage()),
-      )));
+        home: AnimationPage(page: PerfilPage()),
+      ));
 }
